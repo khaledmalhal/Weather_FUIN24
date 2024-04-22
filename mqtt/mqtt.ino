370a77c4c195ac872c8c8e3834971cb9b37508c6
khaledmalhal <khaled.malhal@estudiantat.upc.edu>
2024-04-22 15:45:05 +0200

    Read from DHT11 sensor temperature and humidity

--- mqtt/mqtt.ino
+++ mqtt/mqtt.ino
@@ -3,14 +3,21 @@
 #include <UnoWiFiDevEd.h>
 #include <LiquidCrystal_I2C.h>
 #include <HCSR04.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
 
 // MQTT topic on which we will publish
 #define CONNECTOR "mqtt"
-#define TOPIC "fuin"
+#define TOPIC "weather"
 
-#define PIR   9
-#define ECHO  8
-#define TRIG  7
+#define DHTTYPE    DHT11     // DHT 11
+
+#define DHTPIN  10  // Temperature
+#define PIR     9   // Infrarred
+#define ECHO    8   // Ultrasound
+#define TRIG    7   // Ultrasound
 
 #define LED_GRE   6
 #define LED_YEL   5
@@ -19,13 +26,23 @@
 #define LCD_SCL   3
 #define LCD_SDA   2
 
+bool infra = false;
+bool ultra = false;
+
 LiquidCrystal_I2C lcd(0x27, 20, 4);
+DHT_Unified dht(DHTPIN, DHTTYPE);
 
 void setup(void) {
 	// Serial.begin(9600);
   Serial.begin(115200);
 	Serial.print("Well Come to Arduino MQTT");
  	Ciao.begin();
+  dht.begin();
+
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  dht.humidity().getSensor(&sensor);
+
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_YEL, OUTPUT);
   pinMode(LED_GRE, OUTPUT);
@@ -43,29 +60,55 @@ void setup(void) {
 }
 
 void loop(void) {
-  digitalWrite(LED_RED, HIGH);
-  digitalWrite(LED_YEL, HIGH);
-  digitalWrite(LED_GRE, HIGH);
   String msn = "hello";
   // Serial.println(msn);
   Ciao.write(CONNECTOR, TOPIC, msn);
 
-  delay(5000);
+  delay(1000);
+  lcd.clear();
 
+  // ULTRASOUND
   double *d = HCSR04.measureDistanceCm();
-  for (int i = 0; i < 1; i++) {
-    lcd.setCursor(0, i);
-    lcd.print(i + 1);
-    lcd.print(": ");
-    lcd.print(d[i]);
-    lcd.print(" cm");
-    
-    Serial.print(i + 1);
-    Serial.print(": ");
-    Serial.print(d[i]);
-    Serial.println(" cm");
+  ultra = ((int)d[0] < 20);
+
+  // lcd.setCursor(0, 0);
+  // lcd.print(": ");
+  // lcd.print(d[0]);
+  // lcd.print(" cm");
+
+  if (ultra == true) {
+    lcd.setCursor(0, 0);
+    lcd.print("Vilanova:");
+    lcd.setCursor(0, 1);
+    lcd.print("13.00 C  62.00%");
   }
-  digitalWrite(LED_RED, LOW);
-  digitalWrite(LED_YEL, LOW);
-  digitalWrite(LED_GRE, LOW);
+  else {
+    lcd.setCursor(0, 0);
+    lcd.print("Local:");
+
+    sensors_event_t event;
+    dht.temperature().getEvent(&event);
+
+    if (isnan(event.temperature)) {
+      Serial.println(F("Error reading temperature!"));
+    }
+    else {
+      lcd.setCursor(0, 1);
+      lcd.print(event.temperature);
+      // lcd.setCursor(6, 1);
+      lcd.print("C");
+    }
+
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity)) {
+      Serial.println(F("Error reading humidity!"));
+    }
+    else {
+      lcd.setCursor(9, 1);
+      lcd.print(event.relative_humidity);
+      // lcd.setCursor(15, 1);
+      lcd.print("%");
+    }
+  }
+  delay(750);
 }
